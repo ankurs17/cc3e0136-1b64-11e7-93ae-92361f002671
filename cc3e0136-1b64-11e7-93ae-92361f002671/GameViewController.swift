d08748caf9757c366d9c58807fbf5240c946e8b4
Ankur Saxena <ansaxena@ciena.com>
2017-04-07 00:37:13 -0700

    Initial Commit

---
+++ cc3e0136-1b64-11e7-93ae-92361f002671/GameViewController.swift
@@ -0,0 +1,55 @@
+//
+//  GameViewController.swift
+//  cc3e0136-1b64-11e7-93ae-92361f002671
+//
+//  Created by Saxena, Ankur on 4/7/17.
+//  Copyright © 2017 Saxena, Ankur. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ cc3e0136-1b64-11e7-93ae-92361f002671Tests/cc3e0136_1b64_11e7_93ae_92361f002671Tests.swift
@@ -0,0 +1,36 @@
+//
+//  cc3e0136_1b64_11e7_93ae_92361f002671Tests.swift
+//  cc3e0136-1b64-11e7-93ae-92361f002671Tests
+//
+//  Created by Saxena, Ankur on 4/7/17.
+//  Copyright © 2017 Saxena, Ankur. All rights reserved.
+//
+
+import XCTest
+@testable import cc3e0136_1b64_11e7_93ae_92361f002671
+
+class cc3e0136_1b64_11e7_93ae_92361f002671Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
